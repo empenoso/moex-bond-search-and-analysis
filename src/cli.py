22429025b5fdd7f33a56e2809db220e08bc69e79
Maxim Krivodaev <maxim.krivodaev@gmail.com>
2025-03-10 20:02:14 +0300

    Скрипт поиска новостей перенесен в приложение

--- src/cli.py
+++ src/cli.py
@@ -7,7 +7,7 @@ def start(script_number: None | int = None):
         script_number = int(input(
             "1 - Поиск облигаций по критериям\n"
             "2 - Поиск купонов\n"
-            # "3 - Поиск новостей\n"
+            "3 - Поиск новостей\n"
             # "4 - Подсчет объемов покупки\n"
             "Выберите скрипт: "
         ))
@@ -16,6 +16,7 @@ def start(script_number: None | int = None):
     scripts = {
         1: app.search_by_criteria,
         2: app.search_coupons,
+        3: app.search_news,
     }
     scripts[script_number]()
     print("\nМихаил Шардин https://shardin.name/\n")
